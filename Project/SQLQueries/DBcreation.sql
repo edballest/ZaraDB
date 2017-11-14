77855db47374f6d5cd2eb013751f9e439c6c2c8f
Carlota López Pérez-Arroyo <clopezperezarroyo@hawk.iit.edu>
2017-11-14 13:27:19 -0600

    rest of the tables

--- Project/SQLQueries/DBcreation.sql
+++ Project/SQLQueries/DBcreation.sql
@@ -9,6 +9,14 @@ CREATE TABLE Category(
 type			VARCHAR(20) PRIMARY KEY,
 category		VARCHAR(20))
 
+CREATE TABLE Adresses(
+address_id		CHAR(5) PRIMARY KEY,
+country			VARCHAR(20),
+city			VARCHAR(20),
+street			VARCHAR(20),
+number			VARCHAR(5),
+apartment		VARCHAR(5),
+zip_code		CHAR(5))
 
 ---Store,Customer,Product---
 
@@ -16,11 +24,11 @@ CREATE TABLE Customer(
 customer_id		INT PRIMARY KEY,
 email			NVARCHAR(20) NOT NULL,
 password		NVARCHAR(20) NOT NULL,
-address_id		INT,
+address_id		CHAR(5),
 first_name		NVARCHAR(20),
 middle_name		NVARCHAR(20),
 last_name		NVARCHAR(20),				
-FOREIGN KEY (address_id) REFERENCES Address)
+FOREIGN KEY (address_id) REFERENCES Adresses)
 
 CREATE TABLE Product(
 UPC_code		NVARCHAR(14) PRIMARY KEY,
@@ -31,8 +39,17 @@ size			AS SUBSTRING(UPC_code,10,2),
 color			AS SUBSTRING(UPC_code,12,3),
 FOREIGN KEY (type) REFERENCES Category)
 
+CREATE TABLE Store(
+store_id		CHAR(5) PRIMARY KEY,
+opens			TIME,
+closes			TIME,
+max_inventory	INT not null,
+address_id		CHAR(5),
+FOREIGN KEY (address_id) references Addresses)
 
----Credit Card, Transaction, Suplies, Inventory---
+
+
+---Credit Card, Transaction, Supplies, Inventory---
 
 CREATE TABLE Credit_Card(
 customer_id		INT,
@@ -44,18 +61,27 @@ FOREIGN KEY (customer_id) REFERENCES Customer)
 CREATE TABLE Transact(
 UPC_code		NVARCHAR(14),
 customer_id		INT,
-store_id		INT,
+store_id		CHAR(5),
 quantity		INT,
 date_time		SMALLDATETIME,	
 PRIMARY KEY (UPC_code,customer_id,store_id),
 FOREIGN KEY (UPC_code) REFERENCES Product,
 FOREIGN KEY (customer_id) REFERENCES Customer,
-FOREIGN KEY (store_id) REFERENCES Store,
+FOREIGN KEY (store_id) REFERENCES Store
 )
 
+CREATE TABLE Supplies(
+vendor_name		VARCHAR(20),
+store_id		CHAR(5),
+UPC_code		NVARCHAR(14),
+PRIMARY KEY(vendor_name, store_id, UPC_code),
+FOREIGN KEY (vendor_name) references Vendor,
+FOREIGN KEY (store_id) references Store,
+FOREIGN KEY (UPC_code) references Product
+)
 
 CREATE TABLE Inventory(
-store_id		INT,
+store_id		CHAR(5),
 UPC_code		NVARCHAR(14),
 price			SMALLMONEY,
 quantity		INT,
