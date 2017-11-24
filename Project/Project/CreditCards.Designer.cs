d9e6b28e8e29041376bf62038c11885de0cc59b7
Raquel Buezo <raquelbuezo@gmail.com>
2017-11-23 21:06:34 -0600

    Raquel testing

---
+++ Project/Project/CreditCards.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Project
+{
+    partial class CreditCards
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.creditCardList = new System.Windows.Forms.ListBox();
+            this.add_btn = new System.Windows.Forms.Button();
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.creditCard_txt = new System.Windows.Forms.TextBox();
+            this.back_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // creditCardList
+            // 
+            this.creditCardList.FormattingEnabled = true;
+            this.creditCardList.Location = new System.Drawing.Point(13, 13);
+            this.creditCardList.Name = "creditCardList";
+            this.creditCardList.Size = new System.Drawing.Size(120, 95);
+            this.creditCardList.TabIndex = 0;
+            this.creditCardList.SelectedIndexChanged += new System.EventHandler(this.creditCardList_SelectedIndexChanged);
+            // 
+            // add_btn
+            // 
+            this.add_btn.Location = new System.Drawing.Point(187, 142);
+            this.add_btn.Name = "add_btn";
+            this.add_btn.Size = new System.Drawing.Size(75, 23);
+            this.add_btn.TabIndex = 1;
+            this.add_btn.Text = "Add New";
+            this.add_btn.UseVisualStyleBackColor = true;
+            this.add_btn.Click += new System.EventHandler(this.add_btn_Click);
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.Location = new System.Drawing.Point(153, 13);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(75, 23);
+            this.remove_btn.TabIndex = 2;
+            this.remove_btn.Text = "Remove";
+            this.remove_btn.UseVisualStyleBackColor = true;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_Click);
+            // 
+            // creditCard_txt
+            // 
+            this.creditCard_txt.Location = new System.Drawing.Point(24, 144);
+            this.creditCard_txt.Name = "creditCard_txt";
+            this.creditCard_txt.Size = new System.Drawing.Size(100, 20);
+            this.creditCard_txt.TabIndex = 3;
+            // 
+            // back_btn
+            // 
+            this.back_btn.Location = new System.Drawing.Point(35, 216);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(75, 23);
+            this.back_btn.TabIndex = 4;
+            this.back_btn.Text = "Back";
+            this.back_btn.UseVisualStyleBackColor = true;
+            this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
+            // 
+            // CreditCards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.creditCard_txt);
+            this.Controls.Add(this.remove_btn);
+            this.Controls.Add(this.add_btn);
+            this.Controls.Add(this.creditCardList);
+            this.Name = "CreditCards";
+            this.Text = "CreditCards";
+            this.Load += new System.EventHandler(this.CreditCards_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox creditCardList;
+        private System.Windows.Forms.Button add_btn;
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.TextBox creditCard_txt;
+        private System.Windows.Forms.Button back_btn;
+    }
+}
---
+++ Project/Project/ModifyProfile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class ModifyProfile : Form
+    {
+        public string connectionString;
+        public string customer_id { get; set; }
+        string firstName;
+        string lastName;
+        string email;
+        string password;
+        string country;
+        string city;
+        string street;
+        string number;
+        string apartment;
+        string zipCode;
+
+        public ModifyProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void ModifyProfile_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modify_btn_Click(object sender, EventArgs e)
+        {
+            firstName = firstName_txt.Text.ToString();
+            lastName = lastName_txt.Text.ToString();
+            email = email_txt.Text.ToString();
+            password = pswd_txt.Text.ToString();
+            country = country_txt.Text.ToString();
+            city = city_txt.Text.ToString();
+            street = street_txt.Text.ToString();
+            number = number_txt.Text.ToString();
+            apartment = apt_txt.Text.ToString();
+            zipCode = zipCode_txt.Text.ToString();
+
+            //queries to update tables Customer & Address
+        }
+    }
+}
