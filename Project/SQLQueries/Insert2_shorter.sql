47c7316c2a1c96e3b2a2a6ccb8535c893764045b
Raquel Buezo <raquelbuezo@gmail.com>
2017-11-23 22:00:35 -0600

    Includes: Main, SelectStore, LogIn, Profile, CreditCards, ModifyProfile

---
+++ Project/SQLQueries/Insert2_shorter.sql
@@ -0,0 +1,25 @@
+﻿--Store
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('United States','Springfield','Id Rd.','38718','1','87091'),('United States','Wilmington','Ligula. Road','54676','525','48428'),('United States','Montgomery','Sit Rd.','72743','8','11156');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('Spain','Donosti','Enim. Av.','96126','7','54761'),('Spain','Pamplona','Felis St.','73855','3','19350'),('Spain','Cáceres','Nec St.','19245','9','88035'),('Spain','Santander','Commodo St.','71554','90','04999');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('France','Cambrai','Mauris Road','24868','2','24090'),('France','Nancy','Tellus Avenue','64873','10','42518');
+
+--Customer
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('United States','Sioux City','Nulla Ave','21626','1F','67794'),('United States','Denver','Neque Ave','22148','6E','50214');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('United States','Knoxville','Phasellus Av.','39535','7C','27450'),('United States','Madison','Arcu. Ave','87581','7D','68411');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('United States','Reno','Scelerisque Av.','56126','9D','48961'),('United States','Cleveland','Cursus. Road','23842','7D','40473');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('United States','Topeka','Sem, Av.','33197','4C','54914');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('United States','South Portland','Ante St.','57879','9E','64451'),('United States','Carson City','Amet, St.','88647','8D','28241'),('United States','Los Angeles','Rutrum Av.','71567','2B','68396');
+
+
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('Spain','Ceuta','Dui Rd.','86931','6E','00446'),('Spain','Huesca','Sociis Rd.','44338','2A','11786');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('Spain','Toledo','Sem Av.','79828','1D','21539'),('Spain','Huelva','Ac Av.','59482','6B','67048');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('Spain','Pamplona','Nunc Rd.','14491','2C','51414'),('Spain','Palma de Mallorca','Proin Road','92849','5E','40770');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('Spain','Cuenca','Penatibus Road','78712','9B','06193'),('Spain','Ceuta','Insa St.','85531','8F','33677');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('Spain','Gasteiz','Adipiscing. Ave','96366','2C','06778'),('Spain','Torrevieja','Senectus Rd.','14237','9A','71543'),('Spain','Mataró','Placerat. Road','26151','4C','58759');
+
+
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('France','Lisieux','Dolor Ave','75488','3A','64108');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('France','Biarritz','Dui Rd.','72417','9A','59681');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('France','Castres','Nec Ave','21987','3C','56584'),('France','Perpignan','Eget Street','49166','6D','49922');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('France','Clermont-Ferrand','Pharetra St.','47973','7B','34627'),('France','Arras','Ipsum Av.','51148','6E','83976');
+INSERT INTO Address(country,city,street,number,apartment,zip_code) VALUES('France','Limoges','Urna. St.','53215','9B','81213'),('France','Bègles','Scelerisque Ave','15112','2A','49200'),('France','Rezé','Luctus St.','83861','8D','99304');
