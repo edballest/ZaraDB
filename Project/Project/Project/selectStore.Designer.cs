6792adb4437162fc8acf9c5d4375f7afebfcdfd8
Raquel Buezo <raquelbuezo@gmail.com>
2017-11-23 22:30:30 -0600

    Adding Credit Cards and Modify Profile

---
+++ Project/Project/Project/selectStore.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Project
+{
+    partial class selectStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OK_btn = new System.Windows.Forms.Button();
+            this.cityList = new System.Windows.Forms.ListBox();
+            this.streetList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // OK_btn
+            // 
+            this.OK_btn.Location = new System.Drawing.Point(345, 280);
+            this.OK_btn.Name = "OK_btn";
+            this.OK_btn.Size = new System.Drawing.Size(167, 63);
+            this.OK_btn.TabIndex = 1;
+            this.OK_btn.Text = "OK";
+            this.OK_btn.UseVisualStyleBackColor = true;
+            this.OK_btn.Click += new System.EventHandler(this.OK_btn_Click);
+            // 
+            // cityList
+            // 
+            this.cityList.FormattingEnabled = true;
+            this.cityList.Location = new System.Drawing.Point(44, 40);
+            this.cityList.Name = "cityList";
+            this.cityList.Size = new System.Drawing.Size(212, 225);
+            this.cityList.TabIndex = 2;
+            this.cityList.SelectedIndexChanged += new System.EventHandler(this.cityList_SelectedIndexChanged);
+            // 
+            // streetList
+            // 
+            this.streetList.FormattingEnabled = true;
+            this.streetList.Location = new System.Drawing.Point(300, 40);
+            this.streetList.Name = "streetList";
+            this.streetList.Size = new System.Drawing.Size(212, 225);
+            this.streetList.TabIndex = 3;
+            this.streetList.SelectedIndexChanged += new System.EventHandler(this.streetList_SelectedIndexChanged);
+            // 
+            // selectStore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 379);
+            this.Controls.Add(this.streetList);
+            this.Controls.Add(this.cityList);
+            this.Controls.Add(this.OK_btn);
+            this.Name = "selectStore";
+            this.Text = "selectStore";
+            this.Load += new System.EventHandler(this.selectStore_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OK_btn;
+        private System.Windows.Forms.ListBox cityList;
+        private System.Windows.Forms.ListBox streetList;
+    }
+}
---
+++ Project/Project/SQLQueries/DBcreation.sql
@@ -0,0 +1,91 @@
+﻿---Vendor,Address,Category---
+CREATE TABLE Vendor(
+vendor_name		VARCHAR(20) PRIMARY KEY,
+country			VARCHAR(20),
+delivery_method	VARCHAR(20),
+delivery_time	TINYINT	NOT NULL,
+CHECK (delivery_method IN('truck','train','airplane','boat')))
+
+CREATE TABLE Category(
+type			VARCHAR(20) PRIMARY KEY,
+category		VARCHAR(20))
+
+CREATE TABLE Address(
+address_id		INT PRIMARY KEY IDENTITY,
+country			VARCHAR(20)	NOT NULL,
+city			VARCHAR(20)	NOT NULL,
+street			VARCHAR(20)	NOT NULL,
+number			VARCHAR(5)	NOT NULL,
+apartment		VARCHAR(5),
+zip_code		CHAR(5) NOT NULL)
+
+---Store,Customer,Product---
+
+CREATE TABLE Customer(
+customer_id		INT PRIMARY KEY IDENTITY,
+email			NVARCHAR(20) NOT NULL,
+password		NVARCHAR(20) NOT NULL,
+address_id		INT,
+first_name		NVARCHAR(20),
+middle_name		NVARCHAR(20),
+last_name		NVARCHAR(20),				
+FOREIGN KEY (address_id) REFERENCES Address ON DELETE CASCADE)
+
+CREATE TABLE Product(
+UPC_code		CHAR(14) PRIMARY KEY,
+description		NVARCHAR(50)NOT NULL,
+type			VARCHAR(20) NOT NULL,
+department		VARCHAR(20) NOT NULL,
+item			AS SUBSTRING(UPC_code,1,9),
+size			AS SUBSTRING(UPC_code,10,2),
+color			AS SUBSTRING(UPC_code,12,3),
+FOREIGN KEY (type) REFERENCES Category)
+
+CREATE TABLE Store(
+store_id		INT PRIMARY KEY IDENTITY,
+opens			TIME(0),
+closes			TIME(0),
+max_inventory	INT NOT NULL,
+address_id		INT,
+FOREIGN KEY (address_id) REFERENCES Address)
+
+---Credit Card, Transaction, Supplies, Inventory---
+
+CREATE TABLE Credit_Card(
+customer_id		INT,
+number			CHAR(16),
+PRIMARY KEY (customer_id,number),
+FOREIGN KEY (customer_id) REFERENCES Customer)
+
+CREATE TABLE Transactions(
+UPC_code		CHAR(14),
+customer_id		INT,
+store_id		INT,
+quantity		INT NOT NULL,
+date_time		SMALLDATETIME NOT NULL,	
+PRIMARY KEY (UPC_code,customer_id,store_id),
+FOREIGN KEY (UPC_code) REFERENCES Product,
+FOREIGN KEY (customer_id) REFERENCES Customer,
+FOREIGN KEY (store_id) REFERENCES Store,
+CHECK (quantity > 0))
+
+CREATE TABLE Supplies(
+vendor_name		VARCHAR(20),
+store_id		INT,
+UPC_code		CHAR(14),
+PRIMARY KEY(vendor_name, store_id, UPC_code),
+FOREIGN KEY (vendor_name) references Vendor,
+FOREIGN KEY (store_id) references Store,
+FOREIGN KEY (UPC_code) references Product)
+
+CREATE TABLE Inventory(
+store_id		INT,
+UPC_code		CHAR(14),
+price			SMALLMONEY NOT NULL,
+quantity		INT NOT NULL,
+threshold		INT NOT NULL,
+order_quantity	INT NOT NULL,
+PRIMARY KEY (store_id,UPC_code),
+FOREIGN KEY (store_id) REFERENCES Store,
+FOREIGN KEY (UPC_code) REFERENCES Product)
+
